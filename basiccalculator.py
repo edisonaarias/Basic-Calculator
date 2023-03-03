90cf1fab15d5357bce6350ac85104292f0a1b50d
unknown <edisonalexanderarias@gmail.com>
2023-03-02 23:59:49 -0800

    Added a way to subtract and mulitple

---
+++ basiccalculator.py
@@ -0,0 +1,10 @@
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+op = input('Enter Operator:')
+
+if op == '+':
+    print('The addition is', num1+num2)
+elif op == '-':
+    print('The subtraction is', num1-num2)
+elif
+    print('The multiplication is', num1*num2)
